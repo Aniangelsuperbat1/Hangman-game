8d7f5982b0064af889b48c4b947ec8bd208b82f3
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-12-07 12:46:34 -0500

    Added while loop.

--- hangman.py
+++ hangman.py
@@ -7,4 +7,12 @@ allowed_errors = 2
 guesses = []
 done = False
 
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
 
